261a1e92390a30c951f97326ec5e5a4d14d1979d
Wai Lee <wcl16@ic.ac.uk>
2020-08-02 18:25:56 +0800

    Added styling

--- environment.py
+++ environment.py
@@ -14,26 +14,23 @@ class Environment:
         # Set map
         self.map = map
 
-    def color_cell(self, cell, color):
+    def color_cell(self, cell, color, fill=False):
         width, height = self.map.shape
         x, y = cell
-        p1 = (int(x * self.width / width * self.magnification), int(y * self.height / height * self.magnification))
-        p2 = (int((x + 1) * self.width / width * self.magnification), int((y + 1) * self.height / height * self.magnification))
-        cv2.rectangle(self.image, p1, p2, color, cv2.FILLED)
+        if fill:
+            p1 = (int(x * self.width / width * self.magnification), int(y * self.height / height * self.magnification))
+            p2 = (int((x + 1) * self.width / width * self.magnification), int((y + 1) * self.height / height * self.magnification))
+            cv2.rectangle(self.image, p1, p2, color, cv2.FILLED)
+        else:
+            p = (int((x + 0.5) * self.width / width * self.magnification), int((y + 0.5) * self.height / height * self.magnification))
+            r = int((0.5) * self.width / width * self.magnification)
+            cv2.circle(self.image, p, r, color, cv2.FILLED)
 
     def show(self, queue, visited, path=None):
         # Create white background
         self.image.fill(255)
         # Draw grid
         width, height = self.map.shape
-        for x in range(width):
-            p1 = (int(x * self.width / width * self.magnification), 0)
-            p2 = (int(x * self.width / width * self.magnification), int(self.height * self.magnification))
-            cv2.line(self.image, p1, p2, (0, 0, 0))
-        for y in range(height):
-            p1 = (0, int(y * self.height / height * self.magnification))
-            p2 = (int(self.width * self.magnification), int(y * self.height / height * self.magnification))
-            cv2.line(self.image, p1, p2, (0, 0, 0))
         # Draw starting position
         self.color_cell((0, 0), (0, 0, 255))
         # Draw ending position
@@ -42,18 +39,18 @@ class Environment:
         for y in range(height):
             for x in range(width):
                 if self.map[y][x] == 0:
-                    self.color_cell((x, y), (0, 0, 0))
+                    self.color_cell((x, y), (100, 100, 100), fill=True)
         # Draw path if given
         if path:
             for cell in path:
-                self.color_cell(cell.position, (0, 255, 0))
+                self.color_cell(cell.position, (153, 204, 0))
         else:
             # Draw visited cells
             for cell in visited:
-                self.color_cell(cell.position, (0, 0, 255))
+                self.color_cell(cell.position, (85, 17, 221))
             # Draw to be visit cells
             for cell in queue:
-                self.color_cell(cell.position, (0, 255, 255))
+                self.color_cell(cell.position, (136, 238, 255))
         # Show image
         cv2.imshow("Environment", self.image)
         # Give time for image to be rendered on screen
--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from environment import Environment
 from astar import astar
 
 if __name__ == '__main__':
+    
     path = None
     while path is None:
         # Generate random map
         size = 50
-        map = np.random.randint(2, size=(size, size))
+        # Generate walls
+        map = np.ones((size, size))
+        for i in range(40):
+            map[i][10] = 0
+            map[i][30] = 0
+
+        for i in range(10, 50):
+            map[i][20] = 0
+            map[i][40] = 0
+        # Start and End points
         map[0][0] = map[-1][-1] = 1
         # Create environment
         environment = Environment(map)
